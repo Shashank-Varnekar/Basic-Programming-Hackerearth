c5ed2ce1b62a724aeaafa1d81db4f34692798f24
Shashank Varnekar <47570413+Shashank1311@users.noreply.github.com>
2020-05-13 19:49:11 +0530

    Create A+B.cpp
    This it note my program. I have taken reference from one of the comments below the problem statement.

---
+++ A+B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <boost/multiprecision/cpp_int.hpp>
+using boost::multiprecision::cpp_int;
+using namespace std;
+ 
+int main()
+{
+    cpp_int a, b;
+    while(cin >> a >> b)
+    {
+        cout << a + b << endl;
+    }
+    return 0;
+}
