068b42c88cafefe5e5d657f4b92d253248cbd7e9
Shashank Varnekar <47570413+Shashank1311@users.noreply.github.com>
2020-05-27 20:04:36 +0530

    Create ladderophilia.cpp

---
+++ ladderophilia.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  while(n--){
+    cout<<"*   *"<<endl;
+    cout<<"*   *"<<endl;
+    cout<<"*****"<<endl;
+	}
+    cout<<"*   *"<<endl;
+    cout<<"*   *"<<endl;
+    return 0;
+}
